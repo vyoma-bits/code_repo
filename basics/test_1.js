a3ae2c48b6a1496c8a6771712cb8338dab61bad5
vyoma-bits <145491353+vyoma-bits@users.noreply.github.com>
2024-06-04 15:51:46 +0000

    completed day 1

---
+++ basics/test_1.js
@@ -0,0 +1,35 @@
+const accountId =14452;
+let accountEmail="hitesh@gmail.com"
+var accountpass="12345"
+accountcity="Jaipur"
+accountstate="ji";
+
+// accountId=2 // not allowed
+accountEmail="sc@gmail.com"
+accountpass="1221"
+accountcity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountcity,accountpass,accountstate])
+
+"use strict"; //treat all jd coe as newer version
+//alert(3+3)  we are usng node js and not browser
+let age=18
+let islog=false
+//Datatypes
+//number
+//bigint
+
+//string can be single or double
+
+//boolean true/false
+
+//null standardalone value(empty state)
+
+//undefined 
+
+//symbol  unique
+
+//object
+
+console.log(typeof("hitesh"))
+console.log(typeof null)
